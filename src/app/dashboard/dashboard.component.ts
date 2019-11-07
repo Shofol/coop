3888581c9db8013298dac3f0032cfd7388a68b15
shofol <jahananower@gmail.com>
2019-11-08 02:59:33 +0600

    update dashboard basic

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  date = 'This Month: Oct 1,2019 - Oct 31,2019';
+  tenors = ['Week', 'Month', 'Quarter', 'Year', 'All time'];
+  selectedTenor = 'Month';
   constructor() { }
 
   ngOnInit() {
